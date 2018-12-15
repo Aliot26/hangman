041eb0ac27f45c72430ced1ce35e85a5f7d677df
Aliot26 <olgakochanowa@gmail.com>
2018-12-15 19:43:11 +0100

    Added print functions

---
+++ display.py
@@ -0,0 +1,15 @@
+def print_hello():
+    print('HELLO')
+    print("RULES OF THE GAME")
+
+
+def print_word(word):
+    print(".".join(word))
+
+
+def print_wrong_letters(wrong_letters):
+    print("List of wrong letters: ", wrong_letters)
+
+
+def print_message(msg):
+    print(msg)
