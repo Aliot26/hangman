89a416b34dd27fcf7efb74d2651054d7f0bb5a53
Aliot26 <olgakochanowa@gmail.com>
2018-12-15 20:57:11 +0100

    Added hide word function

--- controller.py
+++ controller.py
@@ -3,6 +3,7 @@ import random
 
 
 filename = "capitals.txt"
+miss_letter = []
 
 
 def choice_word():
@@ -15,3 +16,11 @@ def choice_word():
 def convert_word_to_set_of_letters(word):
     letters_word = [char for char in str(word)]
     return set(letters_word)
+
+
+def hide_word_by_user(word):
+    hide_word = "-"*len(word)
+    print(hide_word)
+
+
+hide_word_by_user("word")
