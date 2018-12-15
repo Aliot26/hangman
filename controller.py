733b068a1b7062fbd351167e4b8302aabefda39c
Aliot26 <olgakochanowa@gmail.com>
2018-12-15 21:34:17 +0100

    Added get letter from user function

--- controller.py
+++ controller.py
@@ -1,9 +1,11 @@
 import file_handling
 import random
-
+import inputs
+import display
 
 filename = "capitals.txt"
-miss_letter = []
+wrong_letters = []
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 
 def choice_word():
@@ -19,8 +21,19 @@ def convert_word_to_set_of_letters(word):
 
 
 def hide_word_by_user(word):
-    hide_word = "-"*len(word)
-    print(hide_word)
+    hide_word = "-" * len(word)
+    return hide_word
 
 
-hide_word_by_user("word")
+def get_letter_from_user():
+    while True:
+        guess = inputs.get_input_string("Please enter a letter ").lower()
+        if len(guess) != 1:
+            display.print_message("There must be single letter")
+        elif guess in wrong_letters:
+            display.print_message(
+                "You have already guessed this letter. Choose again")
+        elif guess not in alphabet:
+            display.print_message("This is not a LETTER")
+        else:
+            return guess
